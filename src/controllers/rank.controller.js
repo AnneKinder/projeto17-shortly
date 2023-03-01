ec3e299101c85c2c58854323a51b0c784a28eba8
AnneKinder <annekhd98@gmail.com>
2023-03-01 12:28:08 -0300

    feat: ranking order by

--- src/controllers/rank.controller.js
+++ src/controllers/rank.controller.js
@@ -2,20 +2,24 @@ import { db } from "../database/database.connection.js";
 
 export async function getRank (req, res) {
 
-
     try{
-
-        const ranking = await db.query(`
-        SELECT users.id, users.name, COUNT(urls.user_id), SUM(urls.qty_visits)
-        FROM urls
-        JOIN users
-        ON users.id=urls.user_id
-        GROUP BY users.id
-        ORDER BY SUM(urls.qty_visits) desc
-        LIMIT 10      
+        const body = await db.query(`
+        SELECT json_build_object(
+            'id', users.id,
+            'name', users.name,
+            'linksCount', COUNT(urls.user_id),
+            'visitCount', SUM(urls.qty_visits)
+        )
+            FROM urls
+            JOIN users
+            ON users.id=urls.user_id
+            GROUP BY users.id
+            ORDER BY SUM(urls.qty_visits) desc
+            LIMIT 10       
         `)
 
-       // res.status(200).send(body)
+       res.status(200).send(body)
+
     
     }
     catch (err) {
